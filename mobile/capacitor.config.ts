c8eb67cce0561279d20e1639d3662332a9d8715c
Andrepeduti <andre.peduti@hotmail.com>
2026-06-07 03:30:08 -0300

    feat: ajuste de nome do app

--- mobile/capacitor.config.ts
+++ mobile/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.andrepeduti.moments',
-  appName: 'Moments',
+  appName: 'Pinless',
   webDir: 'www'
 };
 
